4f790d90171e5aef38e43e03b7387e3bb1c63474
Israt Jahan Mou <139980282+IsratMou@users.noreply.github.com>
2025-04-26 23:10:44 +0600

    home page create

--- protisruti/accounts/views.py
+++ protisruti/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse as HttpsResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpsResponse("hello home page")
