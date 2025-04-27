4299307e3e075d690e9c13035c6b2b73b8664d08
Israt Jahan Mou <139980282+IsratMou@users.noreply.github.com>
2025-04-27 09:05:54 +0600

    signup form fully done

--- protisruti/accounts/views.py
+++ protisruti/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse 
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,10 +11,35 @@ def home(request):
 
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        # Check if passwords match
+        if pass1 != pass2:
+            messages.error(request, "Passwords do not match!")
+            return redirect('signup')
+
+        # Create user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+
+        messages.success(request, "Your account has been created successfully!")
+        return redirect('login')
+
+    # If not POST method, render the signup page
     return render(request, 'signup.html')
+
 
 def login_view(request):
     return render(request, 'login.html')
+
 
 def logout_view(request):
     pass
